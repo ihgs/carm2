fd64deca8f1c320cd27bb0504777f11e5a96650d
ihgs <hgs.dev@gmail.com>
2015-07-19 21:11:11 +0200

    bug fix

--- webapp/app/helpers/admin/students_helper.rb
+++ webapp/app/helpers/admin/students_helper.rb
@@ -1,2 +1,5 @@
 module Admin::StudentsHelper
+  def schools
+    @schools = [{school_id: "", note: ""}] unless @schools
+  end
 end
--- webapp/app/models/student.rb
+++ webapp/app/models/student.rb
@@ -6,6 +6,8 @@ class Student
   field :birthday, type: Date
   field :schools, type: Array  # [{school_id:, note:}]
 
+  before_save :check_school
+
   def age
     return "" unless self.birthday
     birth = self.birthday.strftime("%Y%m%d").to_i
@@ -16,8 +18,29 @@ class Student
   def fullname
     "#{name[:family_name]} #{name[:first_name]}"
   end
-  def schools
-    @schools = [{school_id: "", note: ""}] unless @schools
+
+  def school_list
+    return [] unless self.schools
+
+    self.schools.map do | school |
+      begin
+        school_object = School.find(school[:school_id])
+        name = school_object.name
+      rescue
+        name = "Not Found"
+      end
+
+      {name: name, note: school[:note]}
+    end
+
   end
 
+  protected
+  def check_school
+    if self.schools
+      self.schools = self.schools.select do |school|
+        school[:school_id] and school[:note]
+      end
+    end
+  end
 end
