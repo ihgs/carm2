feb748d5f9b54f36b47ac45177a8689e127c45df
ihgs <hgs.dev@gmail.com>
2015-08-02 22:39:30 +0200

    created repeat form

--- webapp/app/assets/javascripts/admin/dashboard.js
+++ webapp/app/assets/javascripts/admin/dashboard.js
@@ -3,4 +3,5 @@
 //= require bootstrap
 //= require jquery.jpostal
 //= require vis/dist/vis.min
+//= require jquery.repeater/jquery.repeater.js
 //= require_tree .
--- webapp/app/helpers/admin/school_tests_helper.rb
+++ webapp/app/helpers/admin/school_tests_helper.rb
@@ -1,2 +1,8 @@
 module Admin::SchoolTestsHelper
+  def subjects
+    @school_test.subjects = [
+      {name: "", average: "", test_range: ""}
+    ] unless @school_test.subjects
+    @school_test.subjects
+  end
 end
