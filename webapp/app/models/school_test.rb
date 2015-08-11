827a88e5c3e9306a1953cf267fb1ac042d2f1772
ihgs <hgs.dev@gmail.com>
2015-08-11 22:36:37 +0200

    bug fix

--- webapp/app/models/school_test.rb
+++ webapp/app/models/school_test.rb
@@ -3,11 +3,11 @@ class SchoolTest
 
   field :name
   field :start_date, type: Date
-  field :end_date
+  field :end_date, type: Date
   field :subjects, type: Array #[{ name:,average:, test_range: },]
 
   def display_date
-    if end_date.nil? || end_date.empty?
+    if end_date.nil? 
       start_date
     else
       "#{start_date}〜#{end_date}"
