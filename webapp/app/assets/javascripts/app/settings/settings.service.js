8f3248e2bb1638cfcddf59d772ae8223981e2d84
hgs <hgs.dev@gmail.com>
2016-02-25 08:57:28 +0900

    rename app

--- webapp/app/assets/javascripts/app/settings/settings.service.js
+++ webapp/app/assets/javascripts/app/settings/settings.service.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('cramApp')
+angular.module('cram.settings')
   .provider('Course', function () {
     this.$get = ['$resource', function ($resource){
       var Course = $resource('/api/courses/:id', { id: '@id' },{
