d18ca0edddbd1d8980bab88765748857e68f82de
hgs <hgs.dev@gmail.com>
2016-03-19 20:41:42 +0900

    学年によるfilter

--- webapp/app/controllers/api/students_controller.rb
+++ webapp/app/controllers/api/students_controller.rb
@@ -3,7 +3,7 @@ class Api::StudentsController < ApplicationController
   def index
     students = Student.all
     schools = School.all
-
+    grade_num = 100
     response = students.map do |student|
       if student.schools
         student.schools.each do | school |
@@ -11,6 +11,15 @@ class Api::StudentsController < ApplicationController
             if sc.id == school["school_id"]
               school["kind"] = sc.kind
               school["name"] = sc.name
+              _grade = calc_grade(school["enterance_year"], Date.today)
+              if _grade < grade_num
+                grade_num = _grade
+                student["current_school"] = {
+                  "kind": sc.kind,
+                  "name": sc.name,
+                  "grade": grade_num
+                }
+              end
             end
           end
         end
@@ -23,4 +32,11 @@ class Api::StudentsController < ApplicationController
     end
   end
 
+  def calc_grade enterance_year, target_date
+    grade = target_date.year - enterance_year.to_i
+    if target_date.month < 4
+      grade = grade - 1
+    end
+    return grade
+  end
 end
