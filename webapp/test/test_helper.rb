274f5446a6b203442ec6c9b6845a8ea328382bae
hgs <hgs.dev@gmail.com>
2016-05-03 10:49:18 +0900

    add codecov

--- webapp/test/test_helper.rb
+++ webapp/test/test_helper.rb
@@ -1,4 +1,9 @@
 require 'simplecov'
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::Codecov
+]
 SimpleCov.start
 
 ENV['RAILS_ENV'] ||= 'test'
