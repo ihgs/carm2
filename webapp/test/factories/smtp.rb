9e6181c92b1202bc06b6c507489bd8f5cacbf1a3
hgs <hgs.dev@gmail.com>
2017-03-21 20:15:58 +0900

    set default for dev/test env

--- webapp/test/factories/smtp.rb
+++ webapp/test/factories/smtp.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :smtp_config do
     smtp({
-      address: ENV["DOCKER_HOST_IP"],
+      address: ENV["DOCKER_HOST_IP"] || '192.168.33.50',
       port: 1025
       })
   end
